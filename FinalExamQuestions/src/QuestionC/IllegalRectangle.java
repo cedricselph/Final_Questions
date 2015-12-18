45a8a26bc491177e0931041ed07b4557df397b17
Cedric Selph <selphc@udel.edu>
2015-12-18 17:35:43 -0500

    ThanksForAGreatSemester

---
+++ FinalExamQuestions/src/QuestionC/IllegalRectangle.java
@@ -0,0 +1,14 @@
+package QuestionC;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class IllegalRectangle {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
--- FinalExamQuestions/src/QuestionE/Student_Test.java
+++ FinalExamQuestions/src/QuestionE/Student_Test.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.*;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
 
 import org.junit.After;
 import org.junit.AfterClass;
@@ -12,7 +13,8 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-public class Student_Test {
+public class Student_Test{
+	
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
@@ -39,10 +41,24 @@ public class Student_Test {
 		} catch (ParseException e) {
 			e.printStackTrace();
 		}
+		
+		HashMap hMap=new HashMap();
+		
 		Student stu1 = new Student("Bert", "Randall", "Gibbons", dBirthDate, "214 Labrador Lane", "302-893-6889", "bgibbons@udel.edu","MIS");
 
+		Student stu2 = new Student("a","B","C",dBirthDate,"here","302-222-2222","ajhfg@email.com","last");
+		Student stu3 = new Student("d","e","f",dBirthDate,"here","302-222-2222","ajhfg@email.com","last");
+		Student stu4 = new Student("g","h","i",dBirthDate,"here","302-222-2222","ajhfg@email.com","last");
+		Student stu5 = new Student("j","k","l",dBirthDate,"here","302-222-2222","ajhfg@email.com","last");
 
-		assertTrue(1==1);
+		//Mind went blank on how to call their UUID.
+		hMap.put(stu1,1);
+		hMap.put(stu2,2);
+		hMap.put(stu3,3);
+		hMap.put(stu4,4);
+		hMap.put(stu5,5);
+		
+		assertTrue(hMap.size()==5);
 	}
 
 }
